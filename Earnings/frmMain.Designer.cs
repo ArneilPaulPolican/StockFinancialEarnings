bf5f69a6bfc85eb8ba141bf2e023922294fdc5ed
hgminerva <hgminerva@gmail.com>
2018-10-08 16:57:05 +0800

    Glenn update

--- Earnings/frmMain.Designer.cs
+++ Earnings/frmMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Earnings
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -30,11 +30,9 @@
         {
             this.txtDateStart = new System.Windows.Forms.DateTimePicker();
             this.txtDateEnd = new System.Windows.Forms.DateTimePicker();
-            this.BtnSubmit = new System.Windows.Forms.Button();
+            this.btnSubmit = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.txtFileLocation = new System.Windows.Forms.RichTextBox();
-            this.label3 = new System.Windows.Forms.Label();
             this.txtLogActivity = new System.Windows.Forms.RichTextBox();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
@@ -43,7 +41,7 @@
             // txtDateStart
             // 
             this.txtDateStart.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.txtDateStart.Location = new System.Drawing.Point(79, 39);
+            this.txtDateStart.Location = new System.Drawing.Point(79, 12);
             this.txtDateStart.Name = "txtDateStart";
             this.txtDateStart.Size = new System.Drawing.Size(131, 20);
             this.txtDateStart.TabIndex = 0;
@@ -51,25 +49,25 @@
             // txtDateEnd
             // 
             this.txtDateEnd.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.txtDateEnd.Location = new System.Drawing.Point(79, 79);
+            this.txtDateEnd.Location = new System.Drawing.Point(79, 39);
             this.txtDateEnd.Name = "txtDateEnd";
             this.txtDateEnd.Size = new System.Drawing.Size(131, 20);
             this.txtDateEnd.TabIndex = 1;
             // 
-            // BtnSubmit
+            // btnSubmit
             // 
-            this.BtnSubmit.Location = new System.Drawing.Point(67, 180);
-            this.BtnSubmit.Name = "BtnSubmit";
-            this.BtnSubmit.Size = new System.Drawing.Size(75, 23);
-            this.BtnSubmit.TabIndex = 3;
-            this.BtnSubmit.Text = "Start";
-            this.BtnSubmit.UseVisualStyleBackColor = true;
-            this.BtnSubmit.Click += new System.EventHandler(this.BtnSubmit_Click);
+            this.btnSubmit.Location = new System.Drawing.Point(9, 65);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(67, 23);
+            this.btnSubmit.TabIndex = 3;
+            this.btnSubmit.Text = "Start";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 45);
+            this.label1.Location = new System.Drawing.Point(6, 16);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(55, 13);
             this.label1.TabIndex = 4;
@@ -78,67 +76,48 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 85);
+            this.label2.Location = new System.Drawing.Point(6, 41);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(52, 13);
             this.label2.TabIndex = 5;
             this.label2.Text = "Date End";
             // 
-            // txtFileLocation
-            // 
-            this.txtFileLocation.Location = new System.Drawing.Point(79, 122);
-            this.txtFileLocation.Name = "txtFileLocation";
-            this.txtFileLocation.Size = new System.Drawing.Size(131, 43);
-            this.txtFileLocation.TabIndex = 6;
-            this.txtFileLocation.Text = "D:\\";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 125);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(67, 13);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "File Location";
-            // 
             // txtLogActivity
             // 
-            this.txtLogActivity.Location = new System.Drawing.Point(216, 3);
+            this.txtLogActivity.Location = new System.Drawing.Point(9, 94);
             this.txtLogActivity.Name = "txtLogActivity";
-            this.txtLogActivity.Size = new System.Drawing.Size(269, 364);
+            this.txtLogActivity.Size = new System.Drawing.Size(492, 282);
             this.txtLogActivity.TabIndex = 8;
             this.txtLogActivity.Text = "";
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(9, 257);
+            this.progressBar1.Location = new System.Drawing.Point(79, 65);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(201, 23);
+            this.progressBar1.Size = new System.Drawing.Size(422, 23);
             this.progressBar1.TabIndex = 9;
             // 
             // backgroundWorker1
             // 
             this.backgroundWorker1.WorkerReportsProgress = true;
             this.backgroundWorker1.WorkerSupportsCancellation = true;
-            //this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
-            //this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
             // 
-            // Form1
+            // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(501, 366);
+            this.ClientSize = new System.Drawing.Size(511, 383);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.txtLogActivity);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.txtFileLocation);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.BtnSubmit);
+            this.Controls.Add(this.btnSubmit);
             this.Controls.Add(this.txtDateEnd);
             this.Controls.Add(this.txtDateStart);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
-            this.Name = "Form1";
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Yahoo Finance Earning Extractor";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -148,11 +127,9 @@
 
         private System.Windows.Forms.DateTimePicker txtDateStart;
         private System.Windows.Forms.DateTimePicker txtDateEnd;
-        private System.Windows.Forms.Button BtnSubmit;
+        private System.Windows.Forms.Button btnSubmit;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.RichTextBox txtFileLocation;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.RichTextBox txtLogActivity;
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
---
+++ Earnings/frmMain.cs
@@ -0,0 +1,157 @@
+﻿using HtmlAgilityPack;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows.Forms;
+using HtmlDocument = System.Windows.Forms.HtmlDocument;
+
+namespace Earnings
+{
+    public partial class frmMain : Form
+    {
+        public static HtmlDocument doc;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        public void generateEarnings()
+        {
+            List<Earnings> lstEarning = new List<Earnings>();
+
+            WebClient webClient = new WebClient();
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+
+            magentadbDataContext dbProd = new magentadbDataContext();
+
+            DateTime currentDate = Convert.ToDateTime(txtDateStart.Value.ToString("yyyy-MM-dd"));
+            DateTime endDate = Convert.ToDateTime(txtDateEnd.Value.ToString("yyyy-MM-dd"));
+
+            int dateDiff = Convert.ToInt32(endDate.Subtract(currentDate).Days.ToString());
+            int offset = 0;
+
+            progressBar1.Maximum = dateDiff + 1;
+            progressBar1.Value = 0;
+
+            while (currentDate <= endDate)
+            {
+                string urlDateString = currentDate.ToString("yyyy-MM-dd");
+                string url = webClient.DownloadString("https://finance.yahoo.com/calendar/earnings?day=" + urlDateString + "&offset=" + offset);
+
+                doc.LoadHtml(url);
+
+                if (doc.DocumentNode.SelectNodes("//table[contains(@class, 'data-table W(100%) Bdcl(c) Pos(r) BdB Bdc($c-fuji-grey-c)')]") == null)
+                {
+                    txtLogActivity.Text += urlDateString + ": No data. \r\n";
+                }
+                else
+                {
+                    foreach (HtmlNode table in doc.DocumentNode.SelectNodes("//table[contains(@class, 'data-table W(100%) Bdcl(c) Pos(r) BdB Bdc($c-fuji-grey-c)')]"))
+                    {
+                        
+                        foreach (HtmlNode tableBody in table.SelectNodes("tbody"))
+                        {
+
+                            txtLogActivity.Text += urlDateString + ": " + tableBody.SelectNodes("tr").Count() + " symbols \r\n";
+
+                            if (tableBody.SelectNodes("tr").Count() == 100)
+                            {
+                                offset = offset + 100;
+                            }
+                            else
+                            {
+                                offset = 0;
+                            }
+                            
+                            foreach (HtmlNode tableRow in tableBody.SelectNodes("tr"))
+                            {
+
+                                HtmlNodeCollection tableRows = tableRow.SelectNodes("td");
+
+                                if (tableRows.Any())
+                                {
+                                    try
+                                    {
+                                        txtLogActivity.Text += tableRows[1].InnerText + "\r\n";
+                                        txtLogActivity.SelectionStart = txtLogActivity.Text.Length;
+                                        txtLogActivity.ScrollToCaret();
+                                        //txtLogActivity.Text = "Symbol: " + tableRows[1].InnerText + "\r\n" + txtLogActivity.Text;
+                                        //txtLogActivity.Text = "Company: " + tableRows[2].InnerText + "\r\n" + txtLogActivity.Text;
+                                        //txtLogActivity.Text = "Earnings Call Time: " + tableRows[3].InnerText + "\r\n" + txtLogActivity.Text;
+                                        //txtLogActivity.Text = "EPS Estimate: " + tableRows[4].InnerText + "\r\n" + txtLogActivity.Text;
+                                        //txtLogActivity.Text = "Reported EPS: " + tableRows[5].InnerText + "\r\n" + txtLogActivity.Text;
+                                        //txtLogActivity.Text = "Surprise (%): " + tableRows[6].InnerText + "\r\n" + txtLogActivity.Text;
+                                        //txtLogActivity.Text = "\r\n\n" + txtLogActivity.Text;
+
+                                        
+                                        TrnStockEarning newTrnStockEarning = new TrnStockEarning();
+
+                                        string earningSymbol = tableRows[1].InnerText.ToUpper();
+                                        string earningPosition = "Before Market Open";
+
+                                        if (tableRows[3].InnerText.Equals("After Market Close")) earningPosition = "After Market Close";
+
+                                        if (dbProd.TrnStockEarnings.Where(e => e.Symbol == earningSymbol && e.EarningDate.Date == currentDate.Date).Count() == 0)
+                                        {
+                                            var MstSymbol = from s in dbProd.MstSymbols
+                                                            where s.Symbol == earningSymbol && 
+                                                                  (s.Exchange == "NASDAQ" || s.Exchange == "NYSE" || s.Exchange == "AMEX")
+                                                            select new
+                                                            {
+                                                                Id = s.Id,
+                                                            };
+
+                                            if (MstSymbol.Any())
+                                            {
+                                                newTrnStockEarning.Symbol = earningSymbol;
+                                                newTrnStockEarning.SymbolId = MstSymbol.FirstOrDefault().Id;
+                                                newTrnStockEarning.EarningDate = currentDate.Date;
+                                                newTrnStockEarning.EarningTime = earningPosition;
+
+                                                dbProd.TrnStockEarnings.InsertOnSubmit(newTrnStockEarning);
+                                                dbProd.SubmitChanges();
+                                            }
+                                        }
+                                    }
+                                    catch
+                                    {
+                                        txtLogActivity.Text += "Error saving. \r\n";
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                }
+
+                if (offset == 0)
+                {
+                    currentDate = currentDate.AddDays(1);
+                    progressBar1.Value += 1;
+                }
+
+            }
+
+            MessageBox.Show("Completed  ", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            generateEarnings();
+
+        }
+    }
+
+    public class Earnings
+    {
+        public String Symbol { get; set; }
+        public String Company { get; set; }
+        public String EarningsCallTime { get; set; }
+        public String EPSEstimate { get; set; }
+        public String ReportedEPS { get; set; }
+        public String SurprisePercentage { get; set; }
+    }
+}
